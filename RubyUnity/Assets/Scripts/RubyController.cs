aca1fe1e3667326040330143b7e42dc0e7d52001
RaulMonPi <al428109@uji.es>
2022-11-16 18:53:11 +0100

    Sesión 9
    Hemos añadido objetos de curación y vida a Ruby así como enemigos y zona de daño.

--- RubyUnity/Assets/Scripts/RubyController.cs
+++ RubyUnity/Assets/Scripts/RubyController.cs
@@ -1,15 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-   Rigidbody2D rigidbody2d;
+    public float speed = 3.0f;
+    
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    
+    public int health {  get { return currentHealth; }}
+    int currentHealth;
+    bool isInvincible;
+    float invincibleTimer;
+    
+    Rigidbody2D rigidbody2d;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -19,9 +31,32 @@ public class RubyController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 10.0f* horizontal * Time.deltaTime;
-        position.y = position.y + 10.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
